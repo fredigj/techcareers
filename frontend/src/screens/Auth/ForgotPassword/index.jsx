3c561c48a35c71c208b803955108ef7a95d8ed97
Enriko777 <enriko.elezi@fshnstudent.info>
2022-04-02 02:36:06 +0200

    added email validation at ForgotPassword component

--- frontend/src/screens/Auth/ForgotPassword/index.jsx
+++ frontend/src/screens/Auth/ForgotPassword/index.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useLayoutEffect} from 'react'
 import Navbar from '../../../components/Navbar'
 import styles from './ForgotPassword.module.css'
 import { Input, Button, Divider } from '@arco-design/web-react'
@@ -6,7 +6,26 @@ import { Link } from 'react-router-dom'
 import { MdEmail, MdLock } from "react-icons/md";
 
 const ForgotPassword = () => {
+  const [email, setEmail] = useState("");
   const [clicked, setClicked] = useState(false);
+  const [message, setMessage] = useState("");
+
+  const handleClick = () =>{
+    if(email){
+      setClicked(true);
+    }
+  }
+
+  useEffect(() => {
+    const re = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if(clicked){
+      if(email.match(re)){
+        setMessage(`We have sent a password reset link to yout email (${email}).`);
+      }else{
+        setMessage(`(${email}) is an invalid Email.`);
+      }
+    }
+  }, [clicked]);  
 
   return (
     <div>
@@ -16,49 +35,50 @@ const ForgotPassword = () => {
           <p className={styles.title}>
             Forgot Password
           </p>
-          <div style={{
-            display: 'flex',
-            flexDirection: 'column',
-            margin: '25px 0'
-          }}>
-            <label>Email</label>
-            <Input
-              size='large'
-              height={50}
-              style={{marginTop: 10}}
-              // placeholder='Email icon'
-              prefix={<MdEmail />}
-              onClick={() => setClicked(true)}
-              placeholder='Enter your email'
-            />
-          </div>
-          
-          {/* <div style={{
-            width: 'fit-content',
-            marginLeft: 'auto'
-          }}>
-            <Link to="/forgot-password">
-              <p className={styles.link}>Forgot your password?</p>
-            </Link>
-          </div> */}
-          <div>
-              <Button long size='large' type='primary' style={{
-                    margin: '25px 0'
-                }}>
-                    Continue
-              </Button>
-              {/* <Divider style={{
-                width: '20%',
-                margin: '20px auto',
-                minWidth: '20%',
-                maxWidth: '20%'
-              }} />
-              <Button long size='large' type='outline' style={{
-                    margin: '25px 0'
-                }}>
-                    Sign In with Google
-              </Button> */}
-          </div>
+
+          {!clicked ? (
+            <>
+              <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                margin: '25px 0'
+              }}>
+                <label>Email</label>
+                <Input
+                  size='large'
+                  height={50}
+                  style={{marginTop: 10}}
+                  // placeholder='Email icon'
+                  prefix={<MdEmail />}
+                  onChange={(e) => setEmail(e)}
+                  placeholder='Enter your email'
+                />
+              </div>
+              <div>
+                  <Button long size='large' type='primary' style={{
+                        margin: '25px 0'
+                    }}
+                    onClick={handleClick}
+                  >
+                        Continue
+                  </Button>
+              </div>
+            </>
+          ) : (
+            <>
+              <span className="message">{message}</span>
+              <br />
+              <span style={{
+                    fontWeight: 'bold',
+                    cursor: "pointer"
+                }}
+                onClick={() => {setClicked(false); setEmail("")}}
+              >
+                Go back
+              </span>
+            </>
+          )}
+
         </div>
       </div>
     </div>
